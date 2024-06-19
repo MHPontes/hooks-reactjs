1b989f8e3ab6e44a4c3ad87a9c8f682464762e09
MHPontes <mateushspontes@gmail.com>
2024-06-19 16:21:29 -0300

    Iniciando aprendizado sobre hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";  
 
 function App() {
+
+const [tarefas, setTarefas] = useState([
+  'Pagar a conta de luz',
+  'Estudar React Hooks',
+]);
+
+const [input, setInput] = useState('');  //useState é uma função que retorna um array com duas posições, a primeira é o valor da variável e a segunda é uma função para alterar o valor da variável
+
+
+function handleAd() {
+  setTarefas([...tarefas, input]);  //...tarefas é um spread operator, ele pega todos os itens do array tarefas e adiciona a nova tarefa
+  setInput('');  //limpa o input após adicionar a tarefa
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <ul>
+        {tarefas.map(tarefa => (         //map é uma função que percorre o array e retorna um novo array
+          <li key={tarefa}>{tarefa}</li>  //key é um identificador único para cada item da lista
+        ))}
+      </ul>
+
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />  {/*onChange é um evento que dispara a função setInput quando o valor do input é alterado*/}
+      <button type="button" onClick={handleAd} >Adicionar tarefa</button> {/*onClick é um evento que dispara a função handleAd quando o botão é clicado*/}
+      
+      
     </div>
   );
 }
