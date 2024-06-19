ddbe2e01e1ff42601392909984a5001d98326518
MHPontes <mateushspontes@gmail.com>
2024-06-19 16:30:37 -0300

    Aprendendo sobre useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { useState } from "react";  
+import React, { useState, useEffect } from "react";
 
 function App() {
 
-const [tarefas, setTarefas] = useState([
-  'Pagar a conta de luz',
-  'Estudar React Hooks',
-]);
-
-const [input, setInput] = useState('');  //useState é uma função que retorna um array com duas posições, a primeira é o valor da variável e a segunda é uma função para alterar o valor da variável
+  const [tarefas, setTarefas] = useState([]); //useState é uma função que retorna um array com duas posições, a primeira é o valor da variável e a segunda é uma função para alterar o valor da variável
+  const [input, setInput] = useState('');  //useState é uma função que retorna um array com duas posições, a primeira é o valor da variável e a segunda é uma função para alterar o valor da variável
 
 
-function handleAd() {
-  setTarefas([...tarefas, input]);  //...tarefas é um spread operator, ele pega todos os itens do array tarefas e adiciona a nova tarefa
-  setInput('');  //limpa o input após adicionar a tarefa
-}
+  useEffect(() => {  //useEffect é uma função que dispara uma ação toda vez que a página é carregada
+    const tarefasStorage = localStorage.getItem('tarefas');  //getItem é uma função que pega o valor de uma chave no localStorage
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));  //JSON.parse converte a string em array
+    }
+  }, []);  //useEffect é uma função que dispara uma ação toda vez que a página é carregada
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));  //localStorage é um objeto que armazena dados no navegador, JSON.stringify converte o array em string
+  }, [tarefas]);  
+
+  function handleAd() {
+    setTarefas([...tarefas, input]);  //...tarefas é um spread operator, ele pega todos os itens do array tarefas e adiciona a nova tarefa
+    setInput('');  //limpa o input após adicionar a tarefa
+  }
 
   return (
     <div>
@@ -26,8 +33,8 @@ function handleAd() {
 
       <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />  {/*onChange é um evento que dispara a função setInput quando o valor do input é alterado*/}
       <button type="button" onClick={handleAd} >Adicionar tarefa</button> {/*onClick é um evento que dispara a função handleAd quando o botão é clicado*/}
-      
-      
+
+
     </div>
   );
 }
